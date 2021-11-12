c9053ce28c67056df4b050e9f281b681a3013561
IgnacioCon <contrerasign@gmail.com>
2021-11-12 14:32:23 -0600

    added routes to link to profile

--- src/components/ViewButton.js
+++ src/components/ViewButton.js
@@ -1,7 +1,18 @@
+import { Link } from 'react-router-dom';
 import './ViewButton.css';
 
-const ViewButton = () => {
-  return <button className='view-button'>View Profile</button>;
+const ViewButton = ({ user }) => {
+  return (
+    <Link
+      className='view-button'
+      to={{
+        pathname: '/profile',
+        state: { user: user.name, skills: user.skills },
+      }}
+    >
+      View Profile
+    </Link>
+  );
 };
 
 export default ViewButton;
